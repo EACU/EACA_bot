27e98b5b93a37663edd0be2fb834ef4cc6c6e21b
Belvedersky <qwink186@icloud.com>
2019-05-02 13:45:19 +0500

    init

--- EACA_bot/image.js
+++
@@ -1,6 +0,0 @@
-require('custom-env').env();
-
-console.log(process.env.TOKEN);
-var fs = require('fs');
-var text2png = require('text2png');
-fs.writeFileSync('out.png', text2png('Hello!', {color: 'black'}));
